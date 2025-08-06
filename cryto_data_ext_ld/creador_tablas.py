3405cf03f20e012b97bd4f2f6c9c23704c697e23
unknown <carlosmorenobarrado@gmail.com>
2025-08-06 18:26:47 +0200

    Test: Primer proceso funcional

--- cryto_data_ext_ld/creador_tablas.py
+++ cryto_data_ext_ld/creador_tablas.py
@@ -28,20 +28,20 @@ except psycopg2.Error as e:
 
 
 
-conn.execute(''' 
-CREATE TABLE IF NOT EXISTS main.acciones_realizadas (Open_time TIMESTAMP, Decision VARCHAR);
+#cur.execute(''' 
+#CREATE TABLE crypto.acciones_realizadas (Open_time TIMESTAMP, Decision VARCHAR);
+#''')
+#cur.execute('''
+#CREATE TABLE crypto.balance (FechaHora VARCHAR, bitcoin FLOAT, ethereum FLOAT, usdt FLOAT);
+#''')
+#cur.execute('''
+#CREATE TABLE crypto.decision(FechaHora VARCHAR, v_btc FLOAT, v_eth FLOAT, c_btc FLOAT, c_eth FLOAT);
+#''')
+cur.execute('''
+CREATE TABLE crypto.raw_btc_usdt_1m (Open_time TIMESTAMP, Open FLOAT, High FLOAT, Low FLOAT, "Close" FLOAT, Volume FLOAT, Close_time TIMESTAMP, Quote_asset_volume FLOAT, Number_of_trades INTEGER, Taker_buy_base_asset_volume FLOAT, Taker_buy_quote_asset_volume FLOAT, "Ignore" INTEGER);
 ''')
-conn.execute('''
-CREATE TABLE IF NOT EXISTS main.balance (FechaHora VARCHAR, bitcoin FLOAT, ethereum FLOAT, usdt FLOAT);
-''')
-conn.execute('''
-CREATE TABLE IF NOT EXISTS main.decision(FechaHora VARCHAR, v_btc FLOAT, v_eth FLOAT, c_btc FLOAT, c_eth FLOAT);
-''')
-conn.execute('''
-CREATE TABLE IF NOT EXISTS main.raw_btc_usdt_1m (Open_time TIMESTAMP, Open FLOAT, High FLOAT, Low FLOAT, "Close" FLOAT, Volume FLOAT, Close_time TIMESTAMP, Quote_asset_volume FLOAT, Number_of_trades INTEGER, Taker_buy_base_asset_volume FLOAT, Taker_buy_quote_asset_volume FLOAT, "Ignore" INTEGER);
-''')
-conn.execute(''' 
-CREATE TABLE IF NOT EXISTS raw_eth_usdt_1m (Open_time TIMESTAMP, Open FLOAT, High FLOAT, Low FLOAT, "Close" FLOAT, Volume FLOAT, Close_time TIMESTAMP, Quote_asset_volume FLOAT, Number_of_trades INTEGER, Taker_buy_base_asset_volume FLOAT, Taker_buy_quote_asset_volume FLOAT, "Ignore" INTEGER);
+cur.execute(''' 
+CREATE TABLE crypto.raw_eth_usdt_1m (Open_time TIMESTAMP, Open FLOAT, High FLOAT, Low FLOAT, "Close" FLOAT, Volume FLOAT, Close_time TIMESTAMP, Quote_asset_volume FLOAT, Number_of_trades INTEGER, Taker_buy_base_asset_volume FLOAT, Taker_buy_quote_asset_volume FLOAT, "Ignore" INTEGER);
 ''')
 
-conn.close()
+cur.close()
--- cryto_data_ext_ld/main.py
+++ cryto_data_ext_ld/main.py
@@ -1,6 +1,8 @@
 import logging
 import psycopg2
 import os
+from binance import Client
+import pandas as pd
 
 # --- Configuración de Logging ---
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -13,7 +15,7 @@ DB_PORT = "5432"
 SSL_MODE = 'require' 
 
 try:
-    conn = psycopg2.connect(
+    conexion = psycopg2.connect(
         host=DB_HOST,
         database=DB_NAME,
         user=DB_USER,
@@ -21,13 +23,56 @@ try:
         port=DB_PORT,
         #sslmode=SSL_MODE, 
     )
-    cur = conn.cursor()
+    conn = conexion.cursor()
     print(f"Intentando conectar a PostgreSQL en {DB_HOST}:{DB_PORT}...")
     
 except psycopg2.Error as e:
     print(f"Error al conectar o interactuar con la base de datos: {e}")
 
-# Cargar el secreto desde la variable de entorno
-# El nombre 'MI_SECRETO_EN_PYTHON' debe coincidir con el que definiste en el archivo .yml
-api_key = os.environ.get('BINANCE_API_KEY')
-api_secret = os.environ.get('BINANCE_API_SECRET')
+
+#Keys de binance
+api_key = os.environ.get('API_KEY')
+api_secret = os.environ.get('API_SECRET')
+
+client = Client(
+    api_key=api_key, 
+    api_secret=api_secret, 
+    testnet = False
+    )
+
+columns = [
+    "Open_time",
+    "Open",
+    "High",
+    "Low",
+    "Close",
+    "Volume",
+    "Close_time",
+    "Quote_asset_volume",
+    "Number of trades",
+    "Taker_buy_base_asset_volume",
+    "Taker_buy_quote_asset_volume",
+    "Ignore"
+]
+
+delta = 70  #Numero de minutos previos a extraer
+
+try:
+    logging.info("Se inicia el proceso de captura de datos por minuto")
+
+    prices_last_delta_m = client.get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_1MINUTE, f"{delta*2} minutes ago UTC")
+    df_btc = pd.DataFrame(prices_last_delta_m, columns=columns)
+    for col in ["Open_time", "Close_time"]:
+        df_btc[col] = pd.to_datetime(df_btc[col], unit='ms')
+    conn.execute("INSERT INTO main.raw_btc_usdt_1m SELECT * FROM df_btc where Open_time not in (select distinct Open_time from raw_btc_usdt_1m)")
+    prices_last_delta_m = client.get_historical_klines("ETHUSDT", Client.KLINE_INTERVAL_1MINUTE, f"{delta*2} minutes ago UTC")
+    df_eth = pd.DataFrame(prices_last_delta_m, columns=columns)
+    for col in ["Open_time", "Close_time"]:
+        df_eth[col] = pd.to_datetime(df_eth[col], unit='ms')
+    conn.execute("INSERT INTO main.raw_eth_usdt_1m SELECT * FROM df_eth where Open_time not in (select distinct Open_time from raw_eth_usdt_1m)")
+
+except Exception as e:
+    logging.error(f"Parece que hay un error: {e}")
+
+finally:
+    logging.info("Finalizado el proceso")
